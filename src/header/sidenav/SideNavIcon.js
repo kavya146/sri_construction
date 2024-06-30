4a2be585e959d3a8177a6dee72e1054fe9e6956c
kavya singampalli <singampallikavya01@gmail.com>
2024-06-30 14:34:32 +0530

    created vertical menu

--- src/header/sidenav/SideNavIcon.js
+++ src/header/sidenav/SideNavIcon.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidenav.css";
+import { tabs } from "./configs";
 
 const SideNavIcon = () => {
+  const [isOpen, setIsOpen] = useState("false");
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="sidenav-icon-style">
-      {" "}
-      {/* Sidenav icon container */}
-      &#9776; {/* Unicode character for the hamburger icon */}
+    <div>
+      <div className="sidenav-icon-style" onClick={toggleMenu}>
+        &#9776;
+      </div>
+      <div className={isOpen ? "menu-open" : "menu"}>
+        <ul>
+          {tabs.map((x, index) => (
+            <li key={index}>{x}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };

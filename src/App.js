bb3481a9d008a16fb4e88db41f9873807eda912b
kavya singampalli <singampallikavya01@gmail.com>
2024-07-01 13:53:34 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import ContactUs from "./contactUs/ContactUs";
 import Main from "./Main/Main";
 import Header from "./header/Header";
@@ -12,11 +11,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/home" element={<Main />} />
-          <Route path="/about-us" element={<Main />} />
-          <Route path="/our-work" element={<Main />} />
-          <Route path="/services" element={<Main />} />
-          <Route path="/choose-us" element={<Main />} />
+
           <Route path="/contact-us" element={<ContactUs />} />
         </Routes>
       </div>
